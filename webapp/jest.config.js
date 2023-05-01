f6a729d233a4468977f3dc42d30194cba921b862
Manuel López <uo282249@uniovi.es>
2023-05-01 19:45:48 +0200

    push para comprobar testing

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -1,6 +1,5 @@
 export default {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
     testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
     transform: {
         "^.+\\.(js|ts)$": "ts-jest",
@@ -9,5 +8,5 @@ export default {
         "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
         "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
         "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
-    ]
+    ],
 }
