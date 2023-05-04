85921bc4b5088902a8383638df5ead5e2ae4001a
Alicia <aliciafp15@gmail.com>
2023-05-04 22:14:34 +0200

    refactorizacion de codigo  #216

--- webapp/src/components/IconsSidebar/IconsSidebar.test.js
+++ webapp/src/components/IconsSidebar/IconsSidebar.test.js
@@ -2,68 +2,36 @@ import React from 'react';
 import {render, screen,fireEvent} from '@testing-library/react';
 import IconsSidebar from "./IconsSidebar";
 
-
-
 describe('IconsSidebar', () => {
-    it('calls handleSelectedButtonChangeMock when clicking on MyPlaces button',  () => {
-        const handleSelectedButtonChangeMock = jest.fn();
-        render(
-            <IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>
-        );
+  const handleSelectedButtonChangeMock = jest.fn();
+  
+  const testIcon = (iconTestId, expectedButtonName) => {
+    const icon = screen.getByTestId(iconTestId);
+    fireEvent.click(icon);
+    expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith(expectedButtonName);
+  };
 
-        const myPlaces = screen.getByTestId('place-icon');
+  beforeEach(() => {
+    render(<IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>);
+  });
 
-        fireEvent.click(myPlaces);
-        expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith('MyPlaces');
+  it('calls handleSelectedButtonChangeMock when clicking on MyPlaces button', () => {
+    testIcon('place-icon', 'MyPlaces');
+  });
 
+  it('calls handleSelectedButtonChangeMock when clicking on AddPlace button', () => {
+    testIcon('addplace-icon', 'AddPlace');
+  });
 
-    });
-    it('calls handleSelectedButtonChangeMock when clicking on AddPlace button',  () => {
-        const handleSelectedButtonChangeMock = jest.fn();
-        render(
-            <IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>
-        );
+  it('calls handleSelectedButtonChangeMock when clicking on Social button', () => {
+    testIcon('groups-icon', 'Social');
+  });
 
-        const myPlaces = screen.getByTestId('addplace-icon');
+  it('calls handleSelectedButtonChangeMock when clicking on Settings button', () => {
+    testIcon('settings-icon', 'Settings');
+  });
 
-        fireEvent.click(myPlaces);
-        expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith('AddPlace');
-    });
-    it('calls handleSelectedButtonChangeMock when clicking on Social button',  () => {
-        const handleSelectedButtonChangeMock = jest.fn();
-        render(
-            <IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>
-        );
-
-        const myPlaces = screen.getByTestId('groups-icon');
-
-        fireEvent.click(myPlaces);
-        expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith('Social');
-    });
-    it('calls handleSelectedButtonChangeMock when clicking on Settings button',  () => {
-        const handleSelectedButtonChangeMock = jest.fn();
-        render(
-            <IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>
-        );
-
-        const myPlaces = screen.getByTestId('settings-icon');
-
-        fireEvent.click(myPlaces);
-        expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith('Settings');
-    });
-    it('calls handleSelectedButtonChangeMock when clicking on Profile button',  () => {
-        const handleSelectedButtonChangeMock = jest.fn();
-        render(
-            <IconsSidebar handleSelectedButtonChange={handleSelectedButtonChangeMock}/>
-        );
-
-        const myPlaces = screen.getByTestId('profile-icon');
-
-        fireEvent.click(myPlaces);
-        expect(handleSelectedButtonChangeMock).toHaveBeenCalledWith('Profile');
-    });
-
-
-
-
+  it('calls handleSelectedButtonChangeMock when clicking on Profile button', () => {
+    testIcon('profile-icon', 'Profile');
+  });
 });
